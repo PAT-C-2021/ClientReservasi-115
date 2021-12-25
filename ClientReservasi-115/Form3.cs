c4d8b14fc67170d28fbe21be6371dfb28ac7d24c
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmail.com>
2021-12-26 01:35:35 +0700

    Praktikum 11

---
+++ ClientReservasi-115/Form3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientReservasi_115
+{
+    public partial class Login : Form
+    {
+        ServiceReservasi.Service1 service = new ServiceReservasi.Service1();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username = textBoxUsername.Text;
+            string password = textBoxPassword.Text;
+
+            string kategori = service.Login(username, password);
+
+            if (kategori == "Admin")
+            {
+                Register fm = new Register();
+                this.Hide();
+                fm.Show();
+            }
+            else if (kategori == "Resepsionis")
+            {
+                Form1 fm = new Form1();
+                this.Hide();
+                fm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Username dan Password Invalid, Silahkan hubungi admin.");
+                textBoxUsername.Clear();
+                textBoxPassword.Clear();
+            }
+        }
+    }
+}
